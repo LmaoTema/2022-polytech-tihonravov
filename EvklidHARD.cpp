b5ca8e0dbbe59f4ebec2a37addb31dcf521373f0
LmaoTema <tikhonravoff2016@yandex.ru>
2022-09-24 00:08:04 +0300

    EvklidHARD

---
+++ EvklidHARD.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cassert>
+
+int gcd(int a, int b) {
+
+  if (a < 0) a *= -1;
+  if (b < 0) b *= -1;
+
+  // Решение тут
+
+  return 0;
+}
+
+int main() {
+  assert(gcd(0, 5) == 5);
+  assert(gcd(9, 0) == 9);
+  assert(gcd(48, 64) == 16);
+  assert(gcd(-64, 48) == 16);
+  assert(gcd(30, 18) == 6);
+  assert(gcd(-30, -18) == 6);
+  assert(gcd(270, 192) == 6);
+
+  return 0;
+}
