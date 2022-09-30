dbf74368dccbc3f65d5bda7e70fdab7fb7cbd44f
LmaoTema <tikhonravoff2016@yandex.ru>
2022-09-30 22:14:58 +0300

    add project linear

--- 04_search_sort/linear/lin.cpp
+++ 04_search_sort/linear/lin.cpp
@@ -1,41 +1,28 @@
-#include<iostream>
-#include<algorithm>
-using namespace std;
-int search(int arr[], int n, int x, int result[])
-{
-    for (int i = 0; i < n; ++i)
-    {
-        if (arr[i] == x)
-        {
-            return i;
+#include"linfunc.cpp"
 
-        }
-    }
-    return ;  
-}
-int main()
+
+
+int main() 
 {
- int arr[100] = {-996, -960, -944, -887, -865, -860, -846, -831, -790, -785, -700, -691, -662, -662, -647, 
-    -640, -587, -532, -524, -519, -501, -497, -475, -471, -461, -457, -452, -442, -441, -416, 
-    -412, -411, -390, -389, -358, -341, -306, -257, -253, -252, -248, -208, -143, -134, -82, 
-    -79, -11, 21, 26, 48, 68, 69, 72, 93, 95, 99, 115, 120, 156, 164, 181, 200, 231, 252, 288, 
-    304, 336, 343, 364, 402, 419, 486, 546, 584, 595, 596, 606, 627, 636, 666, 666, 683, 701, 
-    740, 766, 780, 782, 794, 802, 829, 832, 837, 858, 875, 881, 882, 883, 883, 947, 959};
-    int x = 120;
-    int t = 0
+    int x;
+    int arr[] = {11, 222, 55, 55, 55};
+    cout << "Element:" << endl;
+    cin >> x;
     int n = sizeof(arr) / sizeof(arr[0]);
+    int result[] = { 11, 222, 55, 55, 55 };
 
-
-    int result =search(arr, n, x);
-    if (result == -10)
+    int num = count(arr, n, x, result);
+    if (num == -1)
     {
-        t + 1;
-        cout << t ;
-    
+        cout << "Element is not present in array" << endl;
     }
-    else 
+    else
     {
-        cout << "dont find it" << result;
+        cout << "There are " << num << " elements" << endl;
+        cout << "Their idexes are " << endl;
+        for (int s = 0; s < num; s++)
+            cout << result[s] << endl;
     }
+    system("pause");
     return 0;
 }
