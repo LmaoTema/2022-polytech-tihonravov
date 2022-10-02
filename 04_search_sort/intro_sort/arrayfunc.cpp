0fcc0ae3e4a416d185df5896d534ce0874c8c37e
LmaoTema <tikhonravoff2016@yandex.ru>
2022-10-03 00:34:47 +0300

    fix: update project arrayfunc

--- 04_search_sort/intro_sort/arrayfunc.cpp
+++ 04_search_sort/intro_sort/arrayfunc.cpp
@@ -1,7 +1,39 @@
 #include <random>
 #include <iostream>
+
+
 using namespace std;
 
+
+void swap(int arr[], int idx_a,int idx_b)
+{
+{
+    int temp = arr[idx_a]; 
+    arr[idx_a] = arr[idx_b];
+    arr[idx_b] = temp; 
+}
+}
+void selection_sort(int arr[], int size)
+{   
+    for (int i = 0; i < size - 1; i++)
+    {
+        
+        int min_index = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        
+        if (min_index != i)
+        {
+          
+            swap(arr, i, min_index);
+        }
+    }
+}
 void fill_array_random(int arr[], int n, int a, int b)
 {
     std::random_device dev;
@@ -13,13 +45,39 @@ void fill_array_random(int arr[], int n, int a, int b)
         arr[i] = dist(rng);
     }
 }
-
-void print_array(int arr[], int n, bool show_index = false)
-{ 
-    for (int i = 0; i < n; i++)
-    cout << arr[i] << '\t';
-    cout << '\n';
+void print_array(int arr[], int n, bool show_index = true)
+{
+    selection_sort(arr, n);
+    if (show_index == false)
+    {
+        cout << "{";
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i];
+            while (i < n)
+            {
+                cout << ",";
+                break;
+            }
+        }
+        cout << "}" << endl;
+    }
+    else 
+    {
+        cout << "{";
+        for (int i = 0; i < n; i++)
+        {
+            cout << i << ":" << arr[i];
+            while (i < n)
+            {
+                cout << ",";
+                break;
+            }
+        }
+        cout << "}" << endl;
+    }    
 }
+
 
 int main()
 {
