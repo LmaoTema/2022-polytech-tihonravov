4fa4b85347e18412cf37f60ee1cfcfe8fcc7f710
LmaoTema <tikhonravoff2016@yandex.ru>
2022-10-06 00:06:22 +0300

    feat(sort_smp): update project cpmSortirovka

--- 04_search_sort/sort_cmp/cmp.cpp
+++ 04_search_sort/sort_cmp/cmp.cpp
@@ -1,65 +1,34 @@
 #include<iostream>
-#include <time.h>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <random>
+#include <cstring>
 
 using namespace std;
 
- void swap(int arr[], int idx_a, int idx_b)
+void fill_array_random(int arr[], int size, int a, int b) 
 {
-    {
-    int temp = arr[idx_a]; 
-    arr[idx_a] = arr[idx_b];
-    arr[idx_b] = temp; 
-    }
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> dist(a, b);
+
+  for (int i = 0; i < size; ++i) {
+    arr[i] = dist(rng);
+  }
 }
 void quick_sort(int arr[], int start, int end)
 {
-    if (start >= end)
+    if (start >= end || start > 0)
+    {
         return;
-
-   
-    int p = partition(arr, start, end);
+    }
+   int p = partition(arr, start, end);
 
  
     quick_sort(arr, start, p - 1); // left part sorty 
-
     quick_sort(arr, p + 1, end); // right part sorty 
 }
-int partition(int arr[], int start, int end)
-{
-
-   
-    int pivot = arr[start];
-
-    
-    int count = 0;
-    for (int i = start + 1; i <= end; i++) {
-        if (arr[i] <= pivot)
-            count++;
-    }
-
-    
-    int pivotIndex = start + count;
-    swap(arr, pivotIndex, start);
-
-
-    int i = start, j = end;
-    while (i < pivotIndex && j > pivotIndex) {
-        while (arr[i] <= pivot) {
-            i++;
-        }
-        while (arr[j] > pivot) {
-            j--;
-        }
-        if (i < pivotIndex && j > pivotIndex) {
-            swap(arr, i, j);
-            i++;
-            j--;
-        }
-    }
-
-    return pivotIndex;
-}
-  
 void selection_sort(int arr[], int size)
 {
 
@@ -83,44 +52,60 @@ void selection_sort(int arr[], int size)
         }
     }
 }
-clock_t start;
-float timing;
-int main()
 
+int partition(int arr[], int start, int end) {
+  int pivot = arr[end];
+  int i = start - 1;
+  for (int j = start; j <= end - 1; ++j) {
+    if (arr[j] <= pivot) {
+      i++;
+      swap(arr, i, j);
+    }
+  }
+  i++;
+  swap(arr, i, end);
+  return i;
+}
+
+int swap(int arr[], int inin, int oout) {
+  int tmp = arr[inin];
+  arr[inin] = arr[oout];
+  arr[oout] = tmp;
+  return arr;
+
+}
+int main()
 { 
-    int size;
-    cout << "Enter array distance number: ";
-    cin >> size;
-    srand(time(NULL));
-    int* a = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        a[i] = rand() % 300 -100;
-    }
-    start = clock();
-    quick_sort(arr, start);
-    cout << "Quick sort: ";
-    for (int i = 0; i < size; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-    timing = (float)(clock() - start) / CLOCKS_PER_SEC;
-    cout << "Quick sort timing = " << timing;
-    cout << endl;
-     
-     
-    start = clock();
-    selection_sort(arr, size);
-    cout << "Selection sort: ";
-    for (int i = 0; i < size; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-    timing = (float)(clock() - start) / CLOCKS_PER_SEC;
-    cout << "Selection sort timing = " << timing;
-    cout << endl;
-     system("pause");
+    long int help[] = {10, 100, 1000, 100000, 100000000};
+
+    for (int i = 0; i != 5; ++i);
+ {
+
+
+    auto size = help[i];
+    
+
+    int arr1[size], arr2[size];
+
+
+    fill_array_random(arr1, size, 0, 100000);
+    memcpy(arr1, arr2, size * sizeof(int));
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    selection_sort(arr1, size);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> ss_ms = t2 - t1;
+
+
+    t1 = std::chrono::high_resolution_clock::now();
+    quick_sort(arr2, 0, size - 1);
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> qs_ms = t2 - t1;
+
+
+    printf("%.0e: %.5f (ss), %.5f (qs)", static_cast<double>(size), ss_ms.count(), qs_ms.count());
+
+
     return 0;
+}
 }
