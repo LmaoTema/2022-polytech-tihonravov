809325052ac0d4ac8a98caa31914a10ad3bf52cb
LmaoTema <tikhonravoff2016@yandex.ru>
2022-11-17 13:03:30 +0300

    fix: update classes project

--- 08_oop/intro/Classes.cpp
+++ 08_oop/intro/Classes.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "String.h"
-using namespace std;
 
 int main () 
 {
     String one = "Radiotechnika";
     String two = one + "= LOVE";
-    cout << two << '\n' << two.get_length();
+    std::cout << two << '\n' << two.get_length();
     return 0;
 }
