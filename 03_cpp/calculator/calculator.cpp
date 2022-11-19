0528cd3950867efde2f832a7719d8da1722a1558
LmaoTema <tikhonravoff2016@yandex.ru>
2022-11-19 11:27:23 +0300

    fix: update CMake & Project

--- 03_cpp/calculator/calculator.cpp
+++ 03_cpp/calculator/calculator.cpp
@@ -1,28 +1,18 @@
-#include<cmath>
-#include <stdio.h>
-#include<iostream>
-using namespace ::std;
+#include <iostream>
+#include "calc.h"
+using namespace std;
+
 int main()
 {
-
-    double a, b, c;
-    char d;
-    while (true)
+    double a, b, result;
+    char $;
+    cout << "Calculator\n Введите данные а и b\n";
+    while(true)
     {
-     
-        cout << "Введите числа без пробелов: " << endl; 
-        cin >> a >> c >> b;
-        switch(d)
-        {
-        case '+': c = a + b; break;
-        case '-': c = a - b; break;
-        case '*': c = a * b; break;
-        case '/': c = a / b; break; // при делени на ноль идёт возврат к началу,то есть к 11 строке.
-        case '^': c = a * b; break;
-        default:
-            cout << "Неверно\n" << endl;
-            continue;
-        }
-        cout << "Результат = ", c << endl;
+        cin >> a >> $ >> b;
+        result = calculate(a, $, b);
+        cout << a << $ << b << "=" << result << endl;
     }
+    return 0;
+
 }
