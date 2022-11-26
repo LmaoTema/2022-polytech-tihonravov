658ddd756243e819ec34be8f6348f15c4b3b7497
LmaoTema <tikhonravoff2016@yandex.ru>
2022-11-26 12:23:54 +0300

    fix: fix palind bag

--- 06_pointers_references/palindrome/polind.cpp
+++ 06_pointers_references/palindrome/polind.cpp
@@ -16,9 +16,9 @@ bool palindrome_check(const char* s)
 int main()
 {
 	assert(palindrome_check("PiliP") == true);
-	assert(palindrome_check("ufo tofu") == true);
+	assert(palindrome_check("ufotofu") == true);
 	assert(palindrome_check("informatika") == false);
-	assert(palindrome_check("Civic") == true);
+	assert(palindrome_check("civic") == true);
 	assert(palindrome_check(" ") == true);
     assert(palindrome_check("Radiotecknika") == false);
 	return  0;
